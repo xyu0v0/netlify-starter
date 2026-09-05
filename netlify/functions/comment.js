ad1d8930e46f7182ba51b1da49714757e9d17f50
xyu0v0 <sedsleep@qq.com>
2026-09-05 17:08:54 +0800

    Update comment.js

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -4,6 +4,14 @@ const serverless = require('serverless-http');
 
 const app = Waline({
   env: 'netlify',
+  // 直接硬编码数据库配置
+  PG_HOST: 'spb-vp0957v64m1tur6k.supabase.opentrust.net',
+  PG_PORT: 5432,
+  PG_DB: 'postgres',
+  PG_USER: 'postgres',
+  PG_PASSWORD: 'm5&&8db&CY',  // 特殊字符在 JS 字符串中没问题
+  PG_PREFIX: 'wl_',
+  PG_SSL: true,
   async postSave(comment) {
     // do what ever you want after save comment
   },
